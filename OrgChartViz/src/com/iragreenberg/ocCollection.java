82830afff1086ff96d7bda9d1e355b4578a0ace1
irajgreenberg <irajgreenberg@gmail.com>
2017-01-08 22:58:28 -0600

    expand progress

--- OrgChartViz/src/com/iragreenberg/ocCollection.java
+++ OrgChartViz/src/com/iragreenberg/ocCollection.java
@@ -14,6 +14,8 @@ public class ocCollection {
 	public float margin = 100.0f;
 	public boolean isSystemHitSafe = true;
 	public boolean isSystemExpandableSafe = true;
+	public float nodeExpansionRate = 3.0f;
+	public float nodeExpansionFactor = 3.0f;
 
 	public ocCollection() {
 	}
@@ -23,10 +25,7 @@ public class ocCollection {
 		this.p = p;
 		float iconW = 60.0f;
 		float gap = (p.width - margin * 2.0f - iconW * entities) / (entities - 1);
-		
-		
-		
-		
+
 		if (entities > 1) {
 			icons.add(new ocIcon(p, new PVector(margin + iconW / 2.0f, p.height / 2.0f), iconW / 2.0f,
 					ocIconDetail.CIRCLE));
@@ -37,8 +36,7 @@ public class ocCollection {
 			icons.add(new ocIcon(p, new PVector(p.width - margin - iconW / 2.0f, p.height / 2.0f), iconW / 2.0f,
 					ocIconDetail.CIRCLE));
 		} else {
-			icons.add(new ocIcon(p, new PVector(p.width/2.0f, p.height / 2.0f),
-					iconW / 2.0f, ocIconDetail.CIRCLE));
+			icons.add(new ocIcon(p, new PVector(p.width / 2.0f, p.height / 2.0f), iconW / 2.0f, ocIconDetail.CIRCLE));
 		}
 
 		for (int i = 0; i < icons.size(); i++) {
@@ -51,7 +49,8 @@ public class ocCollection {
 		for (int i = 0; i < icons.size(); i++) {
 			ocIcon icon = icons.get(i);
 			if (p.mousePressed && icon.isHit() && isSystemHitSafe) {
-				icon.isDraggable = true; // set state NOT actual position of sprite
+				icon.isDraggable = true; // set state NOT actual position of
+											// sprite
 				isSystemHitSafe = false;
 			} else {
 				icon.pos.x = icon.initPos.x + icon.offset.x;
@@ -64,22 +63,35 @@ public class ocCollection {
 				icon.pos.y = p.mouseY;
 				icon.offset = new PVector(p.mouseX - icon.initPos.x, p.mouseY - icon.initPos.y);
 			}
-			
-			if (icon.isHit() && isSystemExpandableSafe) {
-				icon.isExpandable = true; 
-				isSystemExpandableSafe = false;
+
+			if (icon.isHit()) {
+				expand(i);
+				//icon.isExpandable = true;
 			} else {
-				//icon.radius = icon.initRadius;
-				//icon.offset.mult(icon.offsetDamping);
+				icon.radius = icon.initRadius;
 			}
-			
+
 			p.pushMatrix();
-			if (icon.isExpandable) {
-				icon.radius +=1.2f;
-			}
+//			if (icon.isExpandable) {
+//
+//				//expand(i);
+//			}
 
 			icons.get(i).display();
 			p.popMatrix();
+		}
+	}
+
+	// grow all nodes based on proximity to hit node
+	public void expand(int id) {
+		if (icons.get(id).radius < icons.get(id).initRadius * nodeExpansionFactor) {
+			for (int i = 0; i < icons.size(); i++) {
+				if (i == id) {
+					icons.get(i).radius += nodeExpansionRate;
+				} 
+					icons.get(i).radius += nodeExpansionRate;
+
+			}
 		}
 	}
 
@@ -95,7 +107,7 @@ public class ocCollection {
 			icons.get(i).setIsDraggable(isDraggable);
 		}
 	}
-	
+
 	public void setIsExpandable(boolean isExpandable) {
 		if (!isExpandable) {
 			isSystemExpandableSafe = true;
